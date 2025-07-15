e8cbcfc8506c725d6497f93cfe0020bfd18bfeb8
Kyung hyun <jake.roh2000@gmail.com>
2025-07-15 17:41:26 +0900

    feat #3 - 체크리스트 업무 분담을 위한 세팅

--- HomeWorry/src/router/index.js
+++ HomeWorry/src/router/index.js
@@ -1,20 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
-import mapRoutes from './map';
-import analysisRoutes from './analysis';
-import myRoutes from './my';
-import notfoundRoutes from './notfound';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import mapRoutes from "./map";
+import analysisRoutes from "./analysis";
+import myRoutes from "./my";
+import checklistRoutes from "./checklist";
+import notfoundRoutes from "./notfound";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomePage,
     },
     ...mapRoutes,
     ...analysisRoutes,
+    ...checklistRoutes,
     ...myRoutes,
     ...notfoundRoutes,
   ],
