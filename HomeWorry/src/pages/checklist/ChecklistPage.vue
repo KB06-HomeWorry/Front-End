079feaba0c0f7dac4972a28b645aa8ba0041712f
YuBin Kim <yubin412@naver.com>
2025-07-16 11:31:50 +0900

    feat #6 - 체크리스트 상단 step 컴포넌트 구현

--- HomeWorry/src/pages/checklist/ChecklistPage.vue
+++ HomeWorry/src/pages/checklist/ChecklistPage.vue
@@ -1,15 +1,19 @@
 <template>
-  <ChecklistNavBar />
+  <ChecklistNavBar v-model:currentStep="currentStep" />
   체크리스트
   <ChecklistQuestion />
 </template>
 
 <script setup>
-import ChecklistNavBar from "./components/ChecklistNavBar.vue";
-import ChecklistQuestion from "./components/ChecklistQuestion.vue";
-import { useRoute } from "vue-router";
-import ChecklistPage from "@/pages/checklist/ChecklistPage.vue";
-import { ref } from "vue";
+import ChecklistNavBar from './components/ChecklistNavBar.vue';
+import ChecklistQuestion from './components/ChecklistQuestion.vue';
+
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+
 const route = useRoute();
+
+const currentStep = ref(1);
 </script>
+
 <style scoped></style>
