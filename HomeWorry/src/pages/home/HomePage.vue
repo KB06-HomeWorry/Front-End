4dafbc0874eb5b727982718d5391cf11b901f1b4
Lee Dayeon <carrotday0429@gmail.com>
2025-07-16 16:45:01 +0900

    feat #9 - 홈 화면 디렉토리 수정

---
+++ HomeWorry/src/pages/home/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+<ApartBtn
+  text="아파트"
+  icon="/src/assets/icons/home_apartment.png"
+  @click="카테고리선택핸들러"
+/>
+  </main>
+</template>
+
+<script setup>
+    import ApartBtn from '@/pages/home/components/ApartBtn.vue'
+</script>
+ <style></style>
