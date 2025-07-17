02d139c60ccdc8340eac2574781e4209ba83aecb
YuBin Kim <yubin412@naver.com>
2025-07-17 17:39:27 +0900

    feat #7 - 체크리스트 홈에서 type별 연결

--- HomeWorry/src/pages/checklist/ChecklistPage.vue
+++ HomeWorry/src/pages/checklist/ChecklistPage.vue
@@ -1,6 +1,5 @@
 <template>
   <ChecklistNavBar v-model:currentStep="currentStep" />
-  체크리스트
   <ChecklistQuestion />
   <ChecklistBtn
     :currentStep="currentStep"
@@ -15,11 +14,23 @@ import ChecklistQuestion from './components/ChecklistQuestion.vue';
 import ChecklistBtn from './components/ChecklistBtn.vue';
 
 import { useRoute } from 'vue-router';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useChecklistStore } from '@/stores/checklist';
 
 const route = useRoute();
+const checklistStore = useChecklistStore();
 
 const currentStep = ref(1);
+
+onMounted(() => {
+  const { type, stage, userId } = route.query;
+  if (type && stage && userId) {
+    checklistStore.checklistData.type = type;
+    checklistStore.checklistData.stage = stage;
+    checklistStore.checklistData.userId = parseInt(userId);
+    console.log('스토어 초기화 완료:', checklistStore.checklistData);
+  }
+});
 </script>
 
 <style scoped></style>
