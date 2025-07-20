f2fe6bca23b69742c90d6b06aff55e1c586397ab
Lee Dayeon <carrotday0429@gmail.com>
2025-07-20 18:36:34 +0900

    feat/#15 - 로그인/회원가입 필요 컴포넌트 생성 및 스타일 추가

---
+++ HomeWorry/src/pages/auth/components/AuthTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2 class="auth-title titleBold32px">{{ title }}</h2>
+</template>
+
+<script setup>
+    const props = defineProps({ title: String })
+</script>
+
+<style scoped>
+.auth-title {
+    margin-top: 30px;
+    margin-left: 32px;
+    color: var(--color-primary);
+}
+</style>
