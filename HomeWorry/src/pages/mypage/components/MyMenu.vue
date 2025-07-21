9de1c7af9b99859bf6b06afaace2c48b059bf0eb
Lee Dayeon <carrotday0429@gmail.com>
2025-07-21 20:09:55 +0900

    feat #24 - 마이페이지 컴포넌트 생성

--- HomeWorry/src/pages/mypage/components/MyMenu.vue
+++ HomeWorry/src/pages/mypage/components/MyMenu.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="mypage-list-item" @click="$emit('click')">
     <img :src="icon" alt="" class="item-icon" />
-    <span class="item-label">{{ label }}</span>
+    <span class="item-label bodyMedium16px" :class="{ 'delete-label':isDelete}">{{ label }}</span>
+    <div class="item-divider"></div>
   </div>
 </template>
 
 <script setup>
-const props = defineProps({
-  icon: String,
-  label: String,
-})
+  const props = defineProps({
+    icon: String,
+    label: String,
+    isDelete: Boolean
+  })
 </script>
 
 <style scoped>
@@ -18,27 +20,40 @@ const props = defineProps({
   align-items: center;
   gap: 10px;
   padding: 16px 0;
-  font-size: 16px;
-  color: #222;
-  border-bottom: 1px solid #ececec;
   cursor: pointer;
-  background: transparent;
-  transition: background 0.15s;
+  position: relative;
 }
-.mypage-list-item:last-child {
-  border-bottom: none;
-}
-.mypage-list-item.disabled {
-  color: #bbb;
-  cursor: not-allowed;
-}
+
 .item-icon {
-  width: 22px;
-  height: 22px;
+  width: 18px;
+  height: 18px;
+  margin-left: 2rem;
   object-fit: contain;
 }
+
 .item-label {
+  color: var(--color-primary);
+  line-height: 18px;
+  display: flex;
   flex: 1;
-  font-weight: 500;
+  margin-top: 3px;
+}
+
+.item-divider {
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+  width: 360px;
+  height: 0;
+  border-bottom: 1px solid var(--color-light);
+}
+
+.delete-label {
+  color: var(--color-light);
+  transition: color 0.2s;
+}
+.mypage-list-item:hover .delete-label {
+  color: var(--color-primary);
 }
 </style>
