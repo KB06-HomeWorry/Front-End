43d6746acc542e4bc43219c4a0ac8a494850dd69
YuBin Kim <yubin412@naver.com>
2025-07-23 17:05:52 +0900

    feat #44 - 분석 페이지 하단 버튼 구현

--- HomeWorry/src/pages/analysis/AnalysisPage.vue
+++ HomeWorry/src/pages/analysis/AnalysisPage.vue
@@ -9,22 +9,29 @@
       @stageChange="handleStageChange"
     />
     <br />
-    <br />
+
     <StepIncident v-if="currentStep === 1" />
     <StepBuildingHistory v-else-if="currentStep === 2" />
     <StepAgentTrust v-else-if="currentStep === 3" />
     <StepRiskAnalysis v-else-if="currentStep === 4" />
+
+    <!-- 하단 버튼 영역 -->
+    <div class="fixed-footer-btn">
+      <StepButton :text="buttonText" @click="handleButtonClick" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import StepNavigationBar from '@/components/navigation/StepNavigationBar.vue';
 
 import StepIncident from './components/StepIncident.vue';
 import StepBuildingHistory from './components/StepBuildingHistory.vue';
 import StepAgentTrust from './components/StepAgentTrust.vue';
 import StepRiskAnalysis from './components/StepRiskAnalysis.vue';
+
+import StepButton from '@/components/button/BtnMed.vue';
 
 import { useAnalysisStep } from '@/composables/useAnalysisStep';
 
@@ -41,6 +48,37 @@ function handleStageChange(index, stepName) {
   currentStep.value = index + 1;
   setStageByIndex(index);
 }
+
+const buttonText = computed(() => {
+  return currentStep.value === steps.value.length
+    ? '분석 시작하기'
+    : '다음 단계 넘어가기';
+});
+
+function handleButtonClick() {
+  if (currentStep.value === steps.value.length) {
+    startAnalysis();
+  } else {
+    currentStep.value++;
+    setStageByIndex(currentStep.value - 1);
+  }
+}
+
+function startAnalysis() {
+  // TODO: 분석 시작 로직 구현
+  console.log('분석을 시작합니다.');
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.fixed-footer-btn {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 40px;
+  z-index: 100;
+  padding: 12px 0 46px 0;
+  display: flex;
+  justify-content: center;
+}
+</style>
