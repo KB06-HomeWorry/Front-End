3210f618aa2b4188a5986f3b66b0dc1e66618d03
min4034415 <min4034415@gmail.com>
2025-07-25 10:32:58 +0900

    feat#55 - env파일 수정

--- HomeWorry/src/main.js
+++ HomeWorry/src/main.js
@@ -2,11 +2,12 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
 import { useKakao } from 'vue3-kakao-maps/@utils';
-// const kakao_js_api_key = import.meta.env.VITE_KAKAO_JS_API_KEY;
-const kakao_js_api_key = '036a40f4851b27eba70f2401b2329146';
-useKakao(kakao_js_api_key, ['clusterer', 'services', 'drawing']);
+const kakao_js_api_key = useKakao(import.meta.env.VITE_KAKAO_JS_API_KEY, [
+  'clusterer',
+  'services',
+  'drawing',
+]);
 
 import App from './App.vue';
 import router from './router';
