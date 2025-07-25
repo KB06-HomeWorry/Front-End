96eb8057ca6b3f4503013248f093da8ff298bec3
YuBin Kim <yubin412@naver.com>
2025-07-25 13:01:58 +0900

    feat #51 - 서류 분석 버튼 체크리스트에서만 나오게 변경
    위험도 분석 결과 페이지에서

--- HomeWorry/src/pages/dangerResult/components/DangerResultButtons.vue
+++ HomeWorry/src/pages/dangerResult/components/DangerResultButtons.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="button-group">
     <BtnMed
+      v-if="props.moveDefined === 0"
       text="서류 분석"
       color="#ffffff"
       @click="handleClick('documentAnalysis')"
@@ -15,16 +16,16 @@
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
-import { useChecklistStore } from "@/stores/checklist";
-import BtnMed from "@/components/button/BtnMed.vue";
+import { useRouter } from 'vue-router';
+import { useChecklistStore } from '@/stores/checklist';
+import BtnMed from '@/components/button/BtnMed.vue';
 
 const router = useRouter();
-
 const checklistStore = useChecklistStore();
+
 const type = checklistStore.checklistData.type;
-checklistStore.checklistData.stage = "계약 전";
-const stage = "계약 전";
+checklistStore.checklistData.stage = '계약 전';
+const stage = '계약 전';
 const userId = checklistStore.checklistData.userId;
 
 const props = defineProps({
@@ -33,27 +34,28 @@ const props = defineProps({
 
 const handleClick = (action) => {
   switch (action) {
-    case "documentAnalysis":
-      console.log("서류 분석 버튼 클릭됨");
-      router.push("/analysis");
+    case 'documentAnalysis':
+      console.log('서류 분석 버튼 클릭됨');
+      router.push('/analysis');
       break;
-    case "aiContractAnalysis":
-      console.log("AI 계약서 분석 버튼 클릭됨");
+
+    case 'aiContractAnalysis':
+      console.log('AI 계약서 분석 버튼 클릭됨');
       // 여기에 AI 계약서 분석 관련 로직 추가
       break;
     default:
-      console.log("알 수 없는 액션:", action);
+      console.log('알 수 없는 액션:', action);
   }
 };
 
 const checklistAgain = () => {
-  console.log("점검 다시하기 버튼 클릭");
+  console.log('점검 다시하기 버튼 클릭');
   console.log(props.moveDefined);
 
   if (props.moveDefined == 0) {
-    console.log("체크리스트 분석으로 이동");
+    console.log('체크리스트 분석으로 이동');
     router.push({
-      path: "/checklist",
+      path: '/checklist',
       query: {
         type,
         stage,
@@ -61,11 +63,8 @@ const checklistAgain = () => {
       },
     });
   } else if (props.moveDefined == 1) {
-    console.log("서류 분석으로 이동");
-
-    router.push({
-      path: "/analysis",
-    });
+    console.log('서류 분석으로 이동');
+    router.push('/analysis');
   }
 };
 </script>
