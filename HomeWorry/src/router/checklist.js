409c56ef2678dd53f61a6bac26c0c1ae6f8e5728
YuBin Kim <yubin412@naver.com>
2025-07-29 16:41:04 +0900

    feat #79 - 체크리스트 단계 선택 페이지 구현

--- HomeWorry/src/router/checklist.js
+++ HomeWorry/src/router/checklist.js
@@ -1,7 +1,13 @@
 export default [
   {
-    path: "/checklist",
-    name: "checklist",
-    component: () => import("../pages/checklist/ChecklistPage.vue"),
+    path: '/checklist-stage',
+    name: 'ChecklistStage',
+    component: () =>
+      import('../pages/checklist/components/ChecklistStagePage.vue'),
+  },
+  {
+    path: '/checklist',
+    name: 'Checklist',
+    component: () => import('../pages/checklist/ChecklistPage.vue'),
   },
 ];
