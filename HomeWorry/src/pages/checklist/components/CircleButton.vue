c857e6dccf40c04cc0ad2e84d166d48ea0dcef34
YuBin Kim <yubin412@naver.com>
2025-07-30 13:53:39 +0900

    feat #79 - 체크리스트 저장을 위한 CircleButton 컴포넌트 구현

---
+++ HomeWorry/src/pages/checklist/components/CircleButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="circle-btn" @click="$emit('click')">💾</button>
+</template>
+
+<script setup>
+defineEmits(['click']);
+</script>
+
+<style scoped>
+.circle-btn {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  border: none;
+  background-color: #111f5c3a;
+  font-size: 24px;
+  box-shadow: 0 2px 4cap rgba(0, 0, 0, 0.2);
+  transition: background-color 0.2s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
