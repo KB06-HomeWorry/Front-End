5f583350d752eb5a502112761cb19c53fd72934d
Lee Dayeon <carrotday0429@gmail.com>
2025-07-31 14:00:32 +0900

    feat #99 - 퀴즈 헤더 컴포넌트 구현

---
+++ HomeWorry/src/pages/wordquiz/components/QuizHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="quiz-info-bar">
+    <img class="quiz-info-img" src="/src/assets/icons/home_quiz.png" alt="퀴즈 아이콘" />
+    <span class="quiz-info-level titleBold14px">{{ level }}</span>
+    <span class="quiz-info-divider bodyMedium14px">-</span>
+    <span class="quiz-info-number bodyMedium14px">문제 {{ number }}번</span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  imgSrc: { type: String, required: true },       
+  level: { type: String, required: true },         // "초급" | "중급" | "고급"
+  number: { type: [String, Number], required: true }
+})
+</script>
+
+<style scoped>
+.quiz-info-bar {
+    width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 7px; /* 이미지와 텍스트 사이 여백 */
+}
+
+.quiz-info-img {
+  width: 30px;
+  height: 30px;
+  object-fit: contain;
+  margin-right: 4px;
+}
+
+.quiz-info-level {
+  color: var(--color-primary);
+}
+
+.quiz-info-divider {
+  color: var(--color-primary);
+  margin: 0 3px;
+}
+
+.quiz-info-number {
+  color: var(--color-primary);
+}
+</style>
