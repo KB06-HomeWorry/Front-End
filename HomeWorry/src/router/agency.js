5ce141610a0c3f8e29de75121aa9a923c03a0c57
Lee Dayeon <carrotday0429@gmail.com>
2025-08-12 10:53:25 +0900

    chore #195 - 사용하지 않는 agency.js 경로 삭제

--- HomeWorry/src/router/agency.js
+++ HomeWorry/src/router/agency.js
@@ -1,20 +1,10 @@
 export default [
-  // {
-  //   path: '/agencysample/1',
-  //   name: 'agencySample',
-  //   component: () => import('../pages/agency/AgencySample.vue'),
-  // },
   {
     path: '/agency/:agencyId',
     name: 'agencyDetail',
     component: () => import('../pages/agency/AgencyDetail.vue'),
       meta: { hideHeader: true }
   },
-  // {
-  //   path: '/agency/review-write',
-  //   name: 'agencyReviewWrite',
-  //   component: () => import('../pages/agency/AgencyReviewWrite.vue'),
-  // },
   {
     path: '/agency/list',
     name: 'agencyList',
